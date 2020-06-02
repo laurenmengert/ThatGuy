f87858c407fb42172768814c197c50be66d9fe41
Lauren Mengert <laurenmengert@Laurens-MacBook-Pro.local>
2020-06-02 12:52:42 -0600

    Update show view

--- routes/comments.js
+++ routes/comments.js
@@ -3,3 +3,8 @@ var router = express.Router();
 const commentsCtrl = require('../controllers/comments');
 
 router.post('/', isLoggedIn, commentsCtrl.create);
+
+function isLoggedIn(req, res, next) {
+    if ( req.isAuthenticated() ) return next();
+    res.redirect('/auth/google');
+}
